fd430a5745208afdccf43ff3576008ec6573d94b
Vazul25 <Vazul250@gmail.com>
2017-11-19 19:13:47 +0100

    GetBooks

--- Source/CritiquesShelf/CritiquesShelfBLL/Managers/BookManager.cs
+++ Source/CritiquesShelf/CritiquesShelfBLL/Managers/BookManager.cs
@@ -1,12 +1,27 @@
 ﻿using CritiquesShelfBLL.RepositoryInterfaces;
 using CritiquesShelfBLL.Entities;
+using CritiquesShelfBLL.ViewModels;
+using System.Linq;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
 
 namespace CritiquesShelfBLL.Managers
 {
     public class BookManager : RepositoryBase<Book>, IBookRepository
     {
-        public BookManager(CritiquesShelfDbContext context): base(context) {
-            
+        public BookManager(CritiquesShelfDbContext context) : base(context)
+        {
+            List<BookModel> GetBooks(int page = 0, int pageSize = 0)
+            {
+             return   _context.Books.Include(b=>b.TagConnectors).ThenInclude(tc=>tc.Book).Skip(page * pageSize).Take(pageSize).Select(b => new BookModel
+                {
+                    AuthorsNames = b.Authors.Select(a => a.Name).ToList(),
+                    Description = b.Description,
+                    Rateing = b.ReviewScore,
+                    Tags = b.TagConnectors.Select(tc => tc.Tag.Label).ToList(),
+                    Title = b.Title
+                }).ToList();
+            }
         }
 
     }
---
+++ Source/CritiquesShelf/CritiquesShelfBLL/ViewModels/BookModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CritiquesShelfBLL.ViewModels
+{
+    class BookModel
+    {
+        public string Title{ get; set; }
+        public string Description { get; set; }
+        public List<string> AuthorsNames { get; set; }
+        public double Rateing { get; set; }
+        public List<string> Tags { get; set; }
+    }
+}
